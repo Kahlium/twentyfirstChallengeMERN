2ee2156aace0dd82289a8acc7f6e11e3a406d23a
Kahlium <kahligtz@gmail.com>
2024-08-19 22:58:03 -0600

    finished with frontend? server doesnt connect though so checking that

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,35 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
-
 function App() {
   return (
-    <>
-      <ApolloProvider client={client}>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Navbar />
-          <Outlet />
-        </div>
-      </ApolloProvider>
-    </>
+    <ApolloProvider client={client}>
+      <Navbar />
+      <Outlet />
+    </ApolloProvider>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-const routes = require('./routes');
+const express = require('express');
 const path = require('path');
 
-const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
@@ -9,12 +8,13 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+const app = express();
 
 const startApolloServer = async () => {
   await server.start();
@@ -26,14 +26,13 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
 
- // if we're in production, serve client/dist as static assets
- if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-} 
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
   db.once('open', () => {
     app.listen(PORT, () => {
